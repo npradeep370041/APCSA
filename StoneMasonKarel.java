66626ed13e21e9a5be10b88386fa34bfee4344cf
npradeep370041 <143125452+npradeep370041@users.noreply.github.com>
2023-09-01 19:56:35 -0700

    Update StoneMasonKarel.java

--- StoneMasonKarel.java
+++ StoneMasonKarel.java
@@ -1,11 +1,8 @@
 /*
  * File: StoneMasonKarel.java
  * --------------------------
- * The StoneMasonKarel subclass as it appears here does nothing.
- * When you finish writing it, it should solve the "repair the quad"
- * problem from Assignment 1.  In addition to editing the program,
- * you should be sure to edit this comment so that it no longer
- * indicates that the program does nothing.
+ * The StoneMasonKarel subclass should solve the "repair the quad"
+ * problem from Assignment 1.  
  */
 
 import stanford.karel.*;
