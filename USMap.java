be571adc74ec4e83f1eb55e7ca748ba040bdb5c5
npradeep370041 <143125452+npradeep370041@users.noreply.github.com>
2023-09-07 11:34:50 -0700

    Update USMap.java

--- USMap.java
+++ USMap.java
@@ -5,11 +5,27 @@ public class USMap {
 		StdDraw.setPenColor(StdDraw.DARK_GRAY);
 		StdDraw.setPenRadius(0.006);
 		Scanner inFile = FileUtils.openToRead("cities.txt");
+		double[] yCoords = new double[1112];
+		double[] xCoords = new double[1112];
+		String[] cities = new String[1112];
 		while(inFile.hasNextDouble()) {
-			double x = inFile.nextDouble();
-			double y = inFile.nextDouble();
-			StdDraw.point(x,y);
+			for(int i = 0; i < 1112; i++) {
+				yCoords[i] = inFile.nextDouble();
+				xCoords[i] = inFile.nextDouble();
+				StdDraw.point(xCoords[i],yCoords[i]);
+				cities[i] = inFile.nextLine();
+				inFile.nextLine();
+			}
 		}
+		Scanner inFile2 = FileUtils.openToRead("bigCities.txt");
+		String[] bigCities = new String[275];
+		while(inFile2.hasNext()) {
+			for(int j = 0; j < 275; j++) {
+				int useless = inFile2.nextInt();
+				bigCities[j] = inFile2.next();
+			}
+		}
+		System.out.println(bigCities[0]);		
 	}
 		
 	public static void setupCanvas() {
