a1ee8130083a6e3b6e56a737df4ebc82c047c442
npradeep370041 <143125452+npradeep370041@users.noreply.github.com>
2023-09-14 17:04:12 -0700

    Update LetterCount.java

--- LetterCount.java
+++ LetterCount.java
@@ -8,12 +8,14 @@ import java.util.Scanner;
 public class LetterCount {
 	
 	// Fields go here, all must be private
-	int[] letterCount = new int[26];
+	private int[] letterCount;
 	/* Constructor */							
-	public LetterCount() {}
+	public LetterCount() {
+		letterCount = new int[26];
 		for(int init = 0; init < 26; init++) {
 			letterCount[init] = 0;
 		}
+	}
 	/* Main routine */
 	public static void main(String[] args) {
 		LetterCount lc = new LetterCount();
@@ -31,12 +33,39 @@ public class LetterCount {
 	 */
 	public void run(String[] args) {
 		String fileName = args[0];
+		
+		// Reading File and filling out letter array
 		Scanner inFile = FileUtils.openToRead(fileName);
 		while(inFile.hasNext()) {
 			String str = inFile.nextLine().toLowerCase();
 			for(int i = 0; i < str.length(); i++) {
-				char letter = str.charAt(i);
-				
+				int letter = str.charAt(i);
+				if(letter >= 97 && letter <= 122) {
+					letterCount[letter - 97]++;
+				}
+			}
+		}
+		
+		// Finding the maximum number of a letter and finding the value of a plus
+		double max = 0.0;
+		for(int counter = 0; counter < 26; counter++) {
+			if(max < letterCount[counter]) {
+				max = letterCount[counter];
+			}
+		}
+		double plusValue = max/60.0;
+		
+		// Printing out the histogram
+		System.out.println("\nHistogram of letter frequency in " + fileName + "\n");
+		for(int letterData = 0; letterData < 26; letterData++) {
+			System.out.print((char)(letterData + 97) + ":   ");
+			System.out.printf("%4s", letterCount[letterData]);
+			System.out.print(" ");
+			int numPlus = (int)(letterCount[letterData]/plusValue);
+			for(int addPlus = 0; addPlus < numPlus; addPlus++) {
+				System.out.print("+");
+			}
+			System.out.print("\n");
+		}		
 	}
-	
 }
