b19c54a0b598b9bb4e520994fa4b4d91763e3015
npradeep370041 <143125452+npradeep370041@users.noreply.github.com>
2023-09-27 18:23:05 -0700

    Update MVCipher.java

--- MVCipher.java
+++ MVCipher.java
@@ -1,16 +1,33 @@
 // imports go here
 import java.util.Scanner;
+import java.io.PrintWriter;
 /**
- *	MVCipher - Add your description here
+ *	MVCipher - A keyword is entered that is only letters and is more 
+ *  than 3 letters and is used to encrypt or decrypt certain files
  *	Requires Prompt and FileUtils classes.
  *	
- *	@author	
- *	@since	
+ *	@author	Naithik Pradeep
+ *	@since	September 21, 2023
  */
 public class MVCipher {
 	
 	// fields go here
-	private static char[] keyLetters;	
+	
+	// Letters of the key
+	private char[] keyLetters;
+	
+	// ASCII Value of 64 used to get encrypt or decrypt
+	private final int ASCII_VALUE_64 = 64;
+	
+	// Number of letters in the alphabet used to loop around when encrypting or decrypting
+	private final int NUMBER_OF_LETTERS = 26;
+	
+	// Letters used to wrap around the ASCII Alphabet
+	private final int LOWERCASE_A = 97;
+	private final int UPPERCASE_A = 65;
+	private final int LOWERCASE_Z = 122;
+	private final int UPPERCASE_Z = 90;
+	
 	/** Constructor */
 	public MVCipher() {
 
@@ -22,7 +39,7 @@ public class MVCipher {
 	}
 	
 	/**
-	 *	Method header goes here
+	 *	Runs the Cipher Machine
 	 */
 	public void run() {
 		System.out.println("\n Welcome to the MV Cipher machine!\n");
@@ -72,6 +89,10 @@ public class MVCipher {
 			inputFile = Prompt.getString("Name of file to decrypt");
 		}
 		
+		/* Checks to make sure the file exists using a scanner
+		 * Closes the file after checking */
+		Scanner checker = FileUtils.openToRead(inputFile);
+		checker.close();
 		
 		/* Prompt for an output file name */
 		String outputFile = Prompt.getString("Name of output file");
@@ -79,10 +100,11 @@ public class MVCipher {
 		/* Read input file, encrypt or decrypt, and print to output file */
 		if(crypt == 1) {
 			System.out.println("The encrypted file " + outputFile + " has been created using the keyword -> " + key);
-			encrypt(inputFile, outputFile);
+			this.encrypt(inputFile, outputFile);
 		}
 		else {
 			System.out.println("The decrypted file " + outputFile + " has been created using the keyword -> " + key);
+			this.decrypt(inputFile, outputFile);
 		}
 		
 		/* Don't forget to close your output file */
@@ -90,27 +112,97 @@ public class MVCipher {
 	
 	// other methods go here
 	
-	public static void encrypt(String inputFile, String outputFile) {
+	/**
+	 * This method encrypts the file using the keyword
+	 * @param inputFile  name of the file to read
+	 * @param outputFile name of the file to write
+	 */
+	public void encrypt(String inputFile, String outputFile) {
 		Scanner readFile = FileUtils.openToRead(inputFile);
+		PrintWriter writeFile = FileUtils.openToWrite(outputFile);
 		int numLetters = 0;
 		while(readFile.hasNext()) {
 			String line = readFile.nextLine();
 			for(int i = 0; i < line.length(); i++) {
 				char character = line.charAt(i);
-				if((character >= 'A' && character <= 'Z') || (character >= 'a' && character <= 'z')) {
-					character = (char)(character + keyLetters[numLetters] - 64);
+				
+				// Checking if the character is uppercase
+				if(character >= 'A' && character <= 'Z')  {
+					character = (char)(character + keyLetters[numLetters] - ASCII_VALUE_64);
+					if(character > UPPERCASE_Z) {
+						character = (char)(character - NUMBER_OF_LETTERS);
+					}
+					numLetters++;
+					
+					// Resets the letter of the keyword that is being used to encrypt
+					if(numLetters >= keyLetters.length) {
+						numLetters = 0;
+					}
+				}
+				
+				// Checking if the character is lowercase
+				if(character >= 'a' && character <= 'z') {
+					character = (char)(character + keyLetters[numLetters] - ASCII_VALUE_64);
+					if(character > LOWERCASE_Z) {
+						character = (char)(character - NUMBER_OF_LETTERS);
+					}
+					numLetters++;
+					
+					// Resets the letter of the keyword that is being used to encrypt
+					if(numLetters >= keyLetters.length) {
+						numLetters = 0;
+					}
+				}
+				
+				// Adds the character to the file
+				writeFile.print(character);
+			}
+			
+			// Adds next line
+			writeFile.print("\n");
+		}
+		
+		// Closes the file
+		writeFile.close();
+	}
+	
+	/**
+	 * This method decrypts the file using the keyword
+	 * @param inputFile  name of the file to read
+	 * @param outputFile name of the file to write
+	 */
+	public void decrypt(String inputFile, String outputFile) {
+		Scanner readFile = FileUtils.openToRead(inputFile);
+		PrintWriter writeFile = FileUtils.openToWrite(outputFile);
+		int numLetters = 0;
+		while(readFile.hasNext()) {
+			String line = readFile.nextLine();
+			for(int i = 0; i < line.length(); i++) {
+				char character = line.charAt(i);
+				if(character >= 'A' && character <= 'Z') {
+					character = (char)(character - keyLetters[numLetters] + ASCII_VALUE_64);
+					if(character < UPPERCASE_A) {
+						character = (char)(character + NUMBER_OF_LETTERS);
+					}
 					numLetters++;
 					if(numLetters >= keyLetters.length) {
 						numLetters = 0;
 					}
 				}
-				System.out.print(character);
-				
+				if(character >= 'a' && character <= 'z') {
+					character = (char)(character - keyLetters[numLetters] + ASCII_VALUE_64);
+					if(character < LOWERCASE_A) {
+						character = (char)(character + NUMBER_OF_LETTERS);
+					}
+					numLetters++;
+					if(numLetters >= keyLetters.length) {
+						numLetters = 0;
+					}
+				}
+				writeFile.print(character);
 			}
-			System.out.print("\n");
+			writeFile.print("\n");
 		}
-	}
-	public static void decrypt() {
-		
+		writeFile.close();
 	}
 }
