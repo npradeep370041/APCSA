1862b869be107a60e7610928deabf53580fc5299
npradeep370041 <143125452+npradeep370041@users.noreply.github.com>
2023-10-02 09:50:20 -0700

    Update YahtzeeScoreCard.java

--- YahtzeeScoreCard.java
+++ YahtzeeScoreCard.java
@@ -8,6 +8,7 @@ public class YahtzeeScoreCard {
 	
 	private int[] score;
 	private int NUMBER_OF_CATEGORIES = 13;
+	private int NUMBER_OF_DICE = 5;
 	/**
 	 *	Get a category score on the score card.
 	 *	@param category		the category number (1 to 13)
@@ -112,18 +113,70 @@ public class YahtzeeScoreCard {
 	 *
 	 *	@param dg	The DiceGroup to score
 	 */	
-	public void threeOfAKind(DiceGroup dg) {}
+	public void threeOfAKind(DiceGroup dg) {
+		score[6]++;
+		boolean isThreeOfAKind = false;
+		for(int i = 0; i < NUMBER_OF_DICE; i++) {
+			for(int j = 0; j < NUMBER_OF_DICE; j++) {
+				for(int k = 0; k < NUMBER_OF_DICE; k++) {
+					if(i != j && i != k && j != k) {
+						if(dg.getDie(i) == dg.getDie(j) && dg.getDie(i) == dg.getDie(j) && 
+						   dg.getDie(j) == dg.getDie(k)) {
+							isThreeOfAKind = true;
+						}
+					}
+				}
+			}
+		}
+		if(isThreeOfAKind) {
+			score[6] = dg.getTotal();
+		}
+	}
 	
-	public void fourOfAKind(DiceGroup dg) {}
+	public void fourOfAKind(DiceGroup dg) {
+		score[7]++;
+		boolean isFourOfAKind = false;
+		for(int i = 0; i < NUMBER_OF_DICE; i++) {
+			for(int j = 0; j < NUMBER_OF_DICE; j++) {
+				for(int k = 0; k < NUMBER_OF_DICE; k++) {
+					for(int l = 0; l < NUMBER_OF_DICE; l++) {
+						if(i != j && i != k && i != l && j != k && j != l && k != l) {
+							if(dg.getDie(i) == dg.getDie(j) && dg.getDie(i) == dg.getDie(j) && 
+							   dg.getDie(i) == dg.getDie(l) && dg.getDie(j) == dg.getDie(k) && 
+							   dg.getDie(j) == dg.getDie(l) && dg.getDie(k) == dg.getDie(l)) {
+								ifFourOfAKind = true;
+							}
+						}
+					}
+				}
+			}
+		}
+		if(isFourOfAKind) {
+			score[7] = dg.getTotal();
+		}
+	}
 	
-	public void fullHouse(DiceGroup dg) {}
+	public void fullHouse(DiceGroup dg) {
+		
+	}
 	
-	public void smallStraight(DiceGroup dg) {}
+	public void smallStraight(DiceGroup dg) {
+							
+	}
 	
-	public void largeStraight(DiceGroup dg) {}
+	public void largeStraight(DiceGroup dg) {
+		
+	}
 	
-	public void chance(DiceGroup dg) {}
+	public void chance(DiceGroup dg) {
+		score[11] = dg.getTotal();
+	}
 	
-	public void yahtzeeScore(DiceGroup dg) {}
+	public void yahtzeeScore(DiceGroup dg) {
+		if(dg.getDie(0) == dg.getDie(1) && dg.getDie(0) == dg.getDie(2) &&
+		   dg.getDie(0) == dg.getDie(3) && dg.getDie(0) == dg.getDie(4)) {
+			
+		}
+	}
 
 }
